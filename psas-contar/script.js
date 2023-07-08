80f8d3ab08c7dd31c7a90160086ed467cf8c50b3
Pietra Sophia <133168161+pietrao0@users.noreply.github.com>
2023-07-08 00:56:56 +0000

    Fiz correçoes

--- psas-contar/script.js
+++ psas-contar/script.js
@@ -1,17 +1,22 @@
-function contar(){
-    let ini = document.getElementById("txti")
-    let fim = document.getElementById("txtf")
-    let passo = document.getElementById("txtp")
+function contar() {
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
     let res = document.getElementById('res')
 
-    if (ini.ariaValueMax.lenght == 0 || fim.ariaValueMax.lenght == 0 || ipasso.ariaValueMax.lenght == 0 ){
+    if (ini.value.lenght == 0 || fim.value.lenght == 0 || passo.value.lenght == 0 ){
+        res.innerHTML = 'Impossivel contar!'
         window.alert('[ERRO] Faltam dados!')
-    } else{
-        res.innerHTML = 'Contando: '
+        
+    } else {
+        res.innerHTML = 'Contando: <br>'
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
-
+        if (p<=0){
+            window.alert('Passo inválido!  Considerando passo 1')
+            p = 1
+        }
         if(i < f){
             for(let c = i; c <= f; c+=p) {
                 res.innerHTML += `${c} \u{1F92B}`
@@ -20,7 +25,7 @@ function contar(){
  
         } else {
             for(let c = i; c >= f; c-=p) {
-                res.innerHTML += `${c} `
+                res.innerHTML += `${c} \u{1F92B}`
             }
 
         }
