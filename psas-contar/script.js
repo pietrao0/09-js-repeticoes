79c032418bce3b2e6daba646ae64640eb4dda0b4
Pietra Sophia <133168161+pietrao0@users.noreply.github.com>
2023-07-08 16:08:20 +0000

    .

--- psas-contar/script.js
+++ psas-contar/script.js
@@ -2,34 +2,28 @@ function contar() {
     let ini = document.getElementById('txti')
     let fim = document.getElementById('txtf')
     let passo = document.getElementById('txtp')
-    let res = document.getElementById('res')
+    let res = document.getElementById('resultado')
 
-    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0 ){
-        res.innerHTML = 'Impossivel contar!'
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = 'Impossível contar!'
         window.alert('[ERRO] Faltam dados!')
-        
     } else {
         res.innerHTML = 'Contando: <br>'
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
-        if (p <=0 ){
-            window.alert('Passo inválido!  Considerando passo 1')
+        if (p <= 0) {
+            window.alert('Passo inválido! Considerando passo 1')
             p = 1
         }
-        if(i < f){
-            for(let c = i; c <= f; c+=p) {
-                res.innerHTML += ` ${c} \u{1F92B}`
+        if (i < f) {
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += `${c} \u{1F92B} `
             }
-            res.innerHTML += `\u{1F911}`
- 
         } else {
-            for(let c = i; c >= f; c-=p) {
-                res.innerHTML += `${c} \u{1F92B}`
+            for (let c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} \u{1F92B} `
             }
-
         }
-
     }
-
 }
